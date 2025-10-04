2cfc7b1db08c376f72edb7ae6b1339fb480c0235
Kingsley Botchway <kingsley.botchway@amalitech.com>
2025-10-04 09:53:02 +0100

    feat: add

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 80,
+    allowedHosts: ["chain-explorer.mandmservicescorp.org", "localhost"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
